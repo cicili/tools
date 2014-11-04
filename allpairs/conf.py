d8b4661fbdc3e0f45d0b14355b3657ce7f758f50
cicili <cici.x.li@intel.com>
2014-11-04 19:58:00 +0800

    add allpairs tool

---
+++ allpairs/conf.py
@@ -0,0 +1,11 @@
+import sys, os
+import itertools, shutil
+
+
+#os.chdir("/opt/usr/media/tct/opt/wrt-manifest-tizen-tests/") 
+path = os.getcwd()
+path_allpairs = path + "/allpairs"
+seed_file = path + "/input/input_seed.txt"
+selfcomb_file = path + "/output/selfcomb.txt"
+output_file = path + "/output/output.txt"
+
